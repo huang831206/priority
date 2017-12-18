8a7342af86f58b96db97a0edfb450bfa7546f3ff
huang <huang831206@gmail.com>
2017-12-19 00:36:26 +0800

    add list and card partial

---
+++ resources/views/partials/_card.blade.php
@@ -0,0 +1,25 @@
+<script id="card-template" type="text/x-handlebars-template">
+    <div class="ui centered raised card" id="@{{card_id}}">
+        <div class="content">
+            <div class="header">
+                Abbreviated Header
+            </div>
+            <div class="ui fluid action input description">
+                <input type="text" placeholder="text here">
+            </div>
+        </div>
+        <div class="extra content">
+            <div class="right floated author">
+                <i class="user icon"></i> Username
+            </div>
+        </div>
+        <div class="ui two bottom attached buttons">
+            <div class="ui button">
+                Action 1
+            </div>
+            <div class="ui button">
+                Action 2
+            </div>
+        </div>
+    </div>
+</script>
---
+++ resources/views/partials/_list.blade.php
@@ -0,0 +1,15 @@
+<script id="list-template" type="text/x-handlebars-template">
+    <div class="six wide column list-wrapper">
+        <div class="ui card segment centered">
+            <!-- <h4 class="ui top attached inverted header">Header</h4> -->
+            <div class="content" style="background: #545454; color: white;">
+                <i class="right floated large add square icon add-new-card"></i>
+                <div class="header" style="color: white;">Cute Dog</div>
+            </div>
+            <div class="content list-cards" id="@{{list_id}}">
+                <!-- TODO:multi card types -->
+                <!-- a card -->
+            </div>
+        </div>
+    </div>
+</script>
