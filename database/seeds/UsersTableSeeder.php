921aafeefe4521f811c7916c9c152ff18657170e
huang <huang831206@gmail.com>
2017-12-24 10:33:26 +0800

    new dummy user seeder data

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -27,5 +27,33 @@ class UsersTableSeeder extends Seeder
             'password'=>Hash::make('123456'),
             'priority' => '[]',
         ]);
+
+        App\User::create([
+            'email' => 'tom@gmail.com',
+            'name'=>'Tom',
+            'password'=>Hash::make('123456'),
+            'priority' => '[]',
+        ]);
+
+        App\User::create([
+            'email' => 'marry@gmail.com',
+            'name'=>'Marry',
+            'password'=>Hash::make('123456'),
+            'priority' => '[]',
+        ]);
+
+        App\User::create([
+            'email' => 'jerry@gmail.com',
+            'name'=>'Jerry',
+            'password'=>Hash::make('123456'),
+            'priority' => '[]',
+        ]);
+
+        App\User::create([
+            'email' => 'tina@gmail.com',
+            'name'=>'Tina',
+            'password'=>Hash::make('123456'),
+            'priority' => '[]',
+        ]);
     }
 }
